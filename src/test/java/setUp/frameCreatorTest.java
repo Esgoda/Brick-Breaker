983a6cd127077d3d049f728e85a5e2d98355a620
Aoife Slevin <aoife_12@btinternet.com>
2019-10-21 21:59:59 +0100

    Add the frame and panel objects that the game will display on.

---
+++ src/test/java/setUp/frameCreatorTest.java
@@ -0,0 +1,22 @@
+package setUp;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class frameCreatorTest {
+    private mainFrameCreator frame;
+
+    @Test
+    public void createFrameMethod() {
+        //assemble
+        String expected = "Testing Frame";
+        int x = 10;
+        int y = 10;
+        int width = 700;
+        int height = 500;
+        //act
+        frame = new mainFrameCreator(expected, x, y, width, height);
+        //assert
+        assertEquals(expected, frame.getTitle());
+    }
+}
